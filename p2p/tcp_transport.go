b6499c55658b87b473ba1954b2b02e18984f58fa
Sheng-Yan, Zhang <yan.code@gmail.com>
2024-06-08 10:56:25 +0800

    feat: implement methods for NewTCPTransport

--- p2p/tcp_transport.go
+++ p2p/tcp_transport.go
@@ -1,6 +1,7 @@
 package p2p
 
 import (
+	"fmt"
 	"net"
 	"sync"
 )
@@ -17,4 +18,25 @@ func NewTCPTransport(listenAddr string) *TCPTransport {
 	return &TCPTransport{
 		listenAddress: listenAddr,
 	}
+}
+func (t *TCPTransport) ListenAddAccept() error {
+	ln, err := net.Listen("tcp", t.listenAddress)
+	t.listener = ln
+	if err != nil {
+		return err
+	}
+	go t.startAcceptLoop()
+	return nil
+}
+func (t *TCPTransport) startAcceptLoop() {
+	for {
+		conn, err := t.listener.Accept()
+		if err != nil {
+			fmt.Printf("TCP accept error: %v\n", err)
+		}
+		go t.handleConn(conn)
+	}
+}
+func (t *TCPTransport) handleConn(conn net.Conn) {
+	fmt.Printf("new incomming connection: %v\n", conn.RemoteAddr())
 }
--- p2p/tcp_transport_test.go
+++ p2p/tcp_transport_test.go
@@ -7,8 +7,9 @@ import (
 
 func TestTCPTransport(t *testing.T) {
 	//a transport always listens and accepts
-	listAddr := "127.0.0.1:12345"
+	listAddr := ":4000"
 	tr := NewTCPTransport(listAddr)
 	assert.Equal(t, tr.listenAddress, listAddr)
+	assert.Nil(t, tr.ListenAddAccept())
 
 }
